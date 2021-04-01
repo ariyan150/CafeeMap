624f466c4bcdb52b173608f3e5e54b093e0c10a5
Ariyan <a.abidi150@gmail.com>
2021-04-01 06:20:28 +0330

    custom marker - fetch data - marker maker

--- home.js
+++ home.js
@@ -1,46 +1,4 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJpeWFuMTUwIiwiYSI6ImNrbXprYnQ4ODBlbnMydXQ3bnV2ZDUweG0ifQ.lfpDtDAq3YVS8_PNnmJuXQ';
-    var geojson = {
-        'type': 'FeatureCollection',
-        'features': [
-            {
-                'type': 'Feature',
-                'properties': {
-                    'message': 'Foo',
-                    'iconSize': [60, 60]
-                },
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [-66.324462890625, -16.024695711685304]
-                }
-            },
-            {
-                'type': 'Feature',
-                'properties': {
-                    'message': 'Bar',
-                    'iconSize': [50, 50]
-                },
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [-61.2158203125, -15.97189158092897]
-                }
-            },
-            {
-                'type': 'Feature',
-                'properties': {
-                    'message': 'Baz',
-                    'iconSize': [40, 40]
-                },
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [
-                        51.40571594238281,
-                        35.70547135425509
-                      ]
-                }
-            }
-        ]
-    };
-    
     
 
     var map = new mapboxgl.Map({
@@ -50,28 +8,55 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXJpeWFuMTUwIiwiYSI6ImNrbXprYnQ4ODBlbnMydXQ3b
             51.40571594238281,
             35.70547135425509
           ],
-        zoom: 5
+        zoom: 15
     });
+
+
+    fetch("data.json")
+    .then(response => response.json())
+    .then(json => json.features)
+    .then(list => list.map(p => {
+            console.log(p.properties.color)
+            var marker = new mapboxgl.Marker({ color: p.properties.color })
+            .setLngLat(p.geometry.coordinates)
+            .setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
+            .addTo(map);
+    }));
+
+
+
+    const maker = (e) => {
+        var marker = new mapboxgl.Marker({ color: 'black' })
+        .setLngLat([e.lngLat.lng, e.lngLat.lat])
+        .setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
+        .addTo(map);
+    }
+
+
+    map.on('click', maker)
+        
+        // Create a default Marker, colored black, rotated 45 degrees.
+        
 
     // add markers to map
-    geojson.features.forEach(function (marker) {
-        // create a DOM element for the marker
-        var el = document.createElement('div');
-        el.className = 'marker';
-        el.style.backgroundImage =
-            'url(https://placekitten.com/g/' +
-            marker.properties.iconSize.join('/') +
-            '/)';
-        el.style.width = marker.properties.iconSize[0] + 'px';
-        el.style.height = marker.properties.iconSize[1] + 'px';
-        el.style.backgroundSize = '100%';
+    // geojson.features.forEach(function (marker) {
+    //     // create a DOM element for the marker
+    //     var el = document.createElement('div');
+    //     el.className = 'marker';
+    //     el.style.backgroundImage =
+    //         'url(https://placekitten.com/g/' +
+    //         marker.properties.iconSize.join('/') +
+    //         '/)';
+    //     el.style.width = marker.properties.iconSize[0] + 'px';
+    //     el.style.height = marker.properties.iconSize[1] + 'px';
+    //     el.style.backgroundSize = '100%';
 
-        el.addEventListener('click', function () {
-            window.alert(marker.properties.message);
-        });
+    //     el.addEventListener('click', function () {
+    //         window.alert(marker.properties.message);
+    //     });
 
-        // add marker to map
-        new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .addTo(map);
-    });
+    //     // add marker to map
+    //     new mapboxgl.Marker(el)
+    //         .setLngLat(marker.geometry.coordinates)
+    //         .addTo(map);
+    // });
